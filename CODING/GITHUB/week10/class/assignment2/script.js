f87e4cb8d1d4397b47299fad6899259c805dca7b
hrishi008 <www.rishikumawat16@gmail.com>
2022-06-18 17:04:13 +0530

    changes

--- CODING/GITHUB/week10/class/assignment2/script.js
+++ CODING/GITHUB/week10/class/assignment2/script.js
@@ -1,13 +1,16 @@
 // var box= document.querySelector('input');
 var x=true;
+var button=document.getElementById('show');
 var box=document.getElementById('box');
 function password(){
     if(x==true){
         box.setAttribute("type","text");
+        button.innerText='Hide';
         x=false;
     }
    else{
     box.setAttribute("type","password");
+    button.innerText='Show';
     x=true;
    }
 
