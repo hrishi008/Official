473a2ecb0bea6db6bf7d61021d0ce6ffd3e1af6c
hrishi008 <www.rishikumawat16@gmail.com>
2022-08-03 20:13:07 +0530

    changes

---
+++ CODING/GITHUB/week14/project2/script.js
@@ -0,0 +1,5 @@
+console.log('sucess');
+var a=['a','b','c','d','e','f','g',1,2,5,6,8,9,7,4];
+a.sort();
+a.join(".");
+console.log(a);
---
+++ CODING/GITHUB/week14/script.js
@@ -0,0 +1,38 @@
+var btn=document.getElementById('dis');
+
+setTimeout(function(){
+    btn.innerHTML='9';
+},1000)
+setTimeout(function(){
+    btn.innerHTML='8';
+},2000)
+setTimeout(function(){
+    btn.innerHTML='7';
+},3000)
+setTimeout(function(){
+    btn.innerHTML='6';
+},4000)
+setTimeout(function(){
+    btn.innerHTML='5';
+},5000)
+setTimeout(function(){
+    btn.innerHTML='4';
+},6000)
+setTimeout(function(){
+    btn.innerHTML='3';
+},7000)
+setTimeout(function(){
+    btn.innerHTML='2';
+},8000)
+setTimeout(function(){
+    btn.innerHTML='1';
+},9000)
+setTimeout(function(){
+    btn.innerHTML='0';
+},10000)
+setTimeout(function(){
+    let i=0;
+    while(i==0){
+        btn.innerHTML+='10';
+    }
+},11000)
