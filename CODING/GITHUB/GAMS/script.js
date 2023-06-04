a5c48a638e68c26835bef1c720ef72efe975e098
hrishi008 <www.rishikumawat16@gmail.com>
2023-06-04 08:08:13 +0530

    adding timer

--- CODING/GITHUB/GAMS/script.js
+++ CODING/GITHUB/GAMS/script.js
@@ -1,15 +1,15 @@
-// Make a REST callout to a Salesforce Apex REST resource
-function callApexREST() {
-  var url = 'https://cyntexa96-dev-ed.lightning.force.com/services/apexrest/GAMSrestClass';
-  
+// Make a REST callout to Salesforce REST API
+function callSalesforceREST() {
+  var url = 'https://cyntexa96-dev-ed.my.salesforce.com/services/data/v52.0/query?q=SELECT+Id,Name+FROM+Account';
+
   // Create an XMLHttpRequest object
   var xhr = new XMLHttpRequest();
 
-  // Set the request method to GET, POST, PUT, DELETE, etc. depending on your Apex class
+  // Set the request method to GET or other required method
   xhr.open('GET', url, true);
 
-  // Set the request headers (if required)
-  xhr.setRequestHeader('Authorization', 'Bearer 6Cel800D5i00000AGAyc8885i0000011p7gJVVWi4GSitksJaosngyIRANzmmULF2BBRznm0Ya9nibB5DR48FVUvTklvpT7O2vY7oSoZbtz');
+  // Set the request headers
+  xhr.setRequestHeader('Authorization', 'Bearer YOUR_ACCESS_TOKEN');
   xhr.setRequestHeader('Content-Type', 'application/json');
 
   // Set the callback function to handle the response
